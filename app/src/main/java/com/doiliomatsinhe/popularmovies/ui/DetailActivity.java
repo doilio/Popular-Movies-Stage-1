244087e891ac20df20002b231ae2a65cead02ad2
doilio <doiliomatsinhe@hotmail.com>
2020-04-01 16:54:14 +0200

    Initial Commit
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

---
+++ app/src/main/java/com/doiliomatsinhe/popularmovies/ui/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.doiliomatsinhe.popularmovies.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.doiliomatsinhe.popularmovies.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/doiliomatsinhe/popularmovies/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.doiliomatsinhe.popularmovies.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.doiliomatsinhe.popularmovies.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
